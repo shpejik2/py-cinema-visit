bdfd3cccb51f193d9d9428770774a252a26ee043
Melezhyk Oleksandr <melezhyk02@gmail.com>
2026-01-19 18:02:54 +0200

    fix py-cinema-visit

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -9,11 +9,11 @@ class CinemaHall:
     def movie_session(
             self,
             movie_name: str,
-            people: list,
+            customers: list,
             cleaning_staff: Cleaner
     ) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
-        for person in people:
+        for person in customers:
             person.watch_movie(movie_name)
         print(f'"{movie_name}" ended.')
         cleaning_staff.clean_hall(self.number)
