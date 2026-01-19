ee3102edef5ae30c4556082ca19dcd5f7a04b60a
Melezhyk Oleksandr <melezhyk02@gmail.com>
2026-01-19 20:00:47 +0200

    fixed after Luke mistake py-cinema-visit

--- app/main.py
+++ app/main.py
@@ -5,10 +5,10 @@ from app.cinema.hall import CinemaHall
 
 
 def cinema_visit(
-        movie: str,
         customers: list,
         hall_number: int,
-        cleaner: str
+        cleaner: str,
+        movie: str
 ) -> None:
     clients = []
     for client in customers:
